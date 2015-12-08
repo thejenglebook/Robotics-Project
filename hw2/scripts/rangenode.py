d43f58d38710c89df0f97d99591708eff475d637
Gwendolyn Cardiff <mcgwen7@vt.edu>
2015-12-08 20:39:44 +0000

    attempted adding complexities to motornode and trying to repeat readings for rangenode

--- hw2/scripts/rangenode.py
+++ hw2/scripts/rangenode.py
@@ -12,22 +12,26 @@ import math
 #setting up pin
 ADC.setup()
 
+def callback():
+	
+
 #main function reads voltage and publishes Range value
 #ranges reported in cm
 def rangefunc():
+	rospy.init_node('rangefunc', anonymous=True)
 	value = ADC.read("P9_39")
 	outvoltage = 3.3 * value
 	#minimum range
 	if outvoltage > 2.75:
 		rangevalue = 15
-	else if outvoltage < .4:
+	elif outvoltage < .4:
 		rangevalue = 150
 	else:
 		#exponential fit function, derived from data sheet
-		rangevalue = .4 + 3.6 * math.exp(-0.027 * outvoltage)
+		rangevalue = 2000 * math.pow(outvoltage, -1.02)
 
 	#assign values and publish
-	pub = rospy.Publisher('/range', Range)
+	pub = rospy.Publisher('/range', Range, queue_size=10)
 	rangeout = Range()
 	rangeout.min_range = 15
 	rangeout.max_range = 150
@@ -35,8 +39,9 @@ def rangefunc():
 	rospy.loginfo(rangeout)
 	pub.publish(rangeout)
 
+	rospy.Timer(1.0, callback)
 	rospy.spin()
-
+	
 if __name__ == '__main__':
 	try:
 		rangefunc()
